f002a9d7feedf0799fd9bd6c646abfa3e72bd2dd
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-03-25 10:33:26 +0530

    New Class Added

---
+++ src/mar24/Demo1.java
@@ -0,0 +1,21 @@
+package mar24;
+
+public class Demo1 {
+public void add()
+{
+	int a=987,b=98,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=567889,b=67,c;
+	c=a+b;
+	System.out.println(c);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
